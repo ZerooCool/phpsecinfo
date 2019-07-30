61d31f9d3c565e4513d805e20f7237096e48eec2
Zer00CooL <mail@visionduweb.com>
2019-07-30 15:06:15 +0200

    20190730-SecurityInfo-phpsecinfo-v0.2.1.1-ou-v0.2.2.1

---
+++ 20190730-SecurityInfo-phpsecinfo-v0.2.1.1-ou-v0.2.2.1/Menu.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Piwik - free/libre analytics platform
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+namespace Piwik\Plugins\SecurityInfo;
+
+use Piwik\Menu\MenuAdmin;
+use Piwik\Piwik;
+
+class Menu extends \Piwik\Plugin\Menu
+{
+    public function configureAdminMenu(MenuAdmin $menu)
+    {
+        if (Piwik::hasUserSuperUserAccess()) {
+            $menu->addDiagnosticItem(
+                'SecurityInfo_Security',
+                array('module' => 'SecurityInfo', 'action' => 'index'),
+                $order = 10
+            );
+        }
+    }
+}
