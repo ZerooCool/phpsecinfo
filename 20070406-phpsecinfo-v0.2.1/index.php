ff175b3b96c273b978089bc61f4cd3f1dba43472
Zer00CooL <mail@visionduweb.com>
2019-07-31 01:51:43 +0200

    Traduction du fichier index.php #4

--- 20070406-phpsecinfo-v0.2.1/index.php
+++ 20070406-phpsecinfo-v0.2.1/index.php
@@ -1,30 +1,58 @@
-<!-- This is an example page calling the phpsecinfo() function -->
+<!-- ## EN ## This is an example page calling the phpsecinfo() function -->
+<!-- ## FR ## Ceci est un exemple de page appelant la fonction phpsecinfo() -->
 
 <?php require_once('PhpSecInfo/PhpSecInfo.php'); ?>
 <?php phpsecinfo(); ?>
 
 <?php
-/**
+/* ## EN ##
  * If you want to capture output and/or customize the look and feel,
  * you need to do slightly more work.
  *
- * Example:
+ * Example :
  * <code>
  * require_once('PhpSecInfo/PhpSecInfo.php');
- * // instantiate the class
+ * // Instantiate the class :
  * $psi = new PhpSecInfo();
  *
- * // load and run all tests
+ * // Load and run all tests :
  * $psi->loadAndRun();
  *
- * // grab the results as a multidimensional array
+ * // Grab the results as a multidimensional array :
  * $results = $psi->getResultsAsArray();
  * echo "<pre>"; echo print_r($results, true); echo "</pre>";
  *
- * // grab the standard results output as a string
+ * // Grab the standard results output as a string :
  * $html = $psi->getOutput();
  *
- * // send it to the browser
+ * // Send it to the browser :
+ * echo $html;
+ * </code>
+ */
+?>
+
+<?php
+/* ## FR ##
+ * Si vous souhaitez capturer la sortie et/ou personnaliser l'apparence,
+ * vous devez faire un peu plus de travail.
+ *
+ * Exemple :
+ * <code>
+ * require_once('PhpSecInfo/PhpSecInfo.php');
+ * // Instancie la classe :
+ * $psi = new PhpSecInfo();
+ *
+ * // Charge et exécute tous les tests :
+ * $psi->loadAndRun();
+ *
+ * // Récupère les résultats sous forme de tableau multidimensionnel :
+ * $results = $psi->getResultsAsArray();
+ * echo "<pre>"; echo print_r($results, true); echo "</pre>";
+ *
+ * // Récupère les résultats standard sous forme de chaîne :
+ * $html = $psi->getOutput();
+ *
+ * // L'envoie au navigateur
  * echo $html;
  * </code>
  */
