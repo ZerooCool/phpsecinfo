a90f286fd1649133c66db267b9958acdc96a574c
Zer00CooL <mail@visionduweb.com>
2019-07-30 23:40:15 +0200

    securityinfo

---
+++ 20190730-SecurityInfo-phpsecinfo-v0.2.1.1-ou-v0.2.2.1/tests/UI/SecurityInfoReport_spec.js
@@ -0,0 +1,17 @@
+/*!
+ * Piwik - free/libre analytics platform
+ *
+ * Screenshot test for SecurityInfo main page.
+ *
+ * @link http://piwik.org
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+describe("SecurityInfoReport", function () {
+    this.timeout(0);
+
+    it('should load the security info admin page correctly', async function () {
+        await page.goto("?idSite=1&period=year&date=2012-08-09&module=SecurityInfo&tests_hide_piwik_version=1");
+        expect(await page.screenshotSelector('#content')).to.matchImage('admin_security_info');
+    });
+});
