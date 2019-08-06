40a8b88c987e9caa885c86375273b22162ec293c
WinUserNotCooL <mail@visionduweb.com>
2019-08-06 23:41:49 +0200

    #11 Récupération depuis Windows + Commit Push
    Depuis Eclipse sous Windows le caractère de fin de ligne affiché par
    défaut est Windows CLRF.

--- 20070406-phpsecinfo-v0.2.1/PhpSecInfo/Test/Core/gid.php
+++ 20070406-phpsecinfo-v0.2.1/PhpSecInfo/Test/Core/gid.php
@@ -1,28 +1,23 @@
 <?php
 /**
  * Test class for GID
- *
  * @package PhpSecInfo
  * @author Ed Finkler <coj@funkatron.com>
  */
-
 
 /**
  * require the PhpSecInfo_Test_Core class
  */
 require_once('PhpSecInfo/Test/Test_Core.php');
 
-
 /**
  * the minimum "safe" UID that php should be executing as.  This can vary,
  * but in general 100 seems like a good min.
- *
  */
 define('PHPSECINFO_MIN_SAFE_GID', 100);
 
 /**
  * Test class for GID
- *
  * @package PhpSecInfo
  */
 class PhpSecInfo_Test_Core_Gid extends PhpSecInfo_Test_Core
@@ -30,17 +25,13 @@ class PhpSecInfo_Test_Core_Gid extends PhpSecInfo_Test_Core
 
     /**
      * This should be a <b>unique</b>, human-readable identifier for this test
-     *
      * @var string
      */
     var $test_name = "group_id";
-
     var $recommended_value = PHPSECINFO_MIN_SAFE_GID;
-
 
     /**
      * This test only works under Unix OSes
-     *
      * @return boolean
      */
     function isTestable()
@@ -67,7 +58,6 @@ class PhpSecInfo_Test_Core_Gid extends PhpSecInfo_Test_Core
 
     /**
      * Checks the GID of the PHP process to make sure it is above PHPSECINFO_MIN_SAFE_GID
-     *
      * @see PHPSECINFO_MIN_SAFE_GID
      */
     function _execTest()
@@ -79,15 +69,12 @@ class PhpSecInfo_Test_Core_Gid extends PhpSecInfo_Test_Core
         return PHPSECINFO_TEST_RESULT_WARN;
     }
 
-
     /**
      * Set the messages specific to this test
-     *
      */
     function _setMessages()
     {
         parent::_setMessages();
-
         $this->setMessageForResult(PHPSECINFO_TEST_RESULT_OK, 'en', 'PHP is executing as what is probably a non-privileged group');
         $this->setMessageForResult(PHPSECINFO_TEST_RESULT_WARN, 'en', 'PHP may be executing as a "privileged" group, which could be a serious security vulnerability.');
         $this->setMessageForResult(PHPSECINFO_TEST_RESULT_NOTRUN, 'en', 'This test will not run on Windows OSes');
