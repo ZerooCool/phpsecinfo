db89dd401c5f3fed6aacbf26fdb970ac6e1bceae
WinUserNotCooL <mail@visionduweb.com>
2019-08-08 14:49:53 +0200

    #11 Création d'un fichier depuis Eclipse sous Windows
    Test de création de fichier depuis Eclipse sous Windows.

---
+++ 20070406-phpsecinfo-v0.2.1/PhpSecInfo/Test/Core/test_fin_de_ligne.php
@@ -0,0 +1,7 @@
+<?php 
+echo("Test de fin de ligne Windows");
+echo("Git a été installé et configuré avec l'installeur");
+echo("Création d'un fichier depuis Eclipse qui utilise la fin de ligne Windows");
+echo("Suite au commit push on ne souhaite pas avoir la fin de ligne Windows");
+echo("Depuis Github :");
+echo("L'historique du fichier ne doit pas afficher le caractère rouge de fin de ligne");
