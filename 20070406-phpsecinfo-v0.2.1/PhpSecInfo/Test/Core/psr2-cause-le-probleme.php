a68c38aa4d68e77e3a8858579eb4dc5a6850791b
WinUserNotCooL <mail@visionduweb.com>
2019-08-09 03:32:40 +0200

    #11 dmg

--- 20070406-phpsecinfo-v0.2.1/PhpSecInfo/Test/Core/psr2-cause-le-probleme.php
+++ 20070406-phpsecinfo-v0.2.1/PhpSecInfo/Test/Core/psr2-cause-le-probleme.php
@@ -10,11 +10,9 @@
  */
 require_once ('PhpSecInfo/Test/Test.php');
 
-
  */
 class PhpSecInfo_Test_Session extends PhpSecInfo_Test
 {
-
     public $test_group = 'Session';
 
     /**
@@ -27,7 +25,6 @@ class PhpSecInfo_Test_Session extends PhpSecInfo_Test
     {
         return true;
     }
-    // Erreur uniquement sous PHP !
-    // Le code n'était pas fermé avec la balise php fermante. PSR-2 veut alors faire un espace forcé et attend la suite du code !!!
+    // Hé bien non :/ !!!
 }
 ?>
