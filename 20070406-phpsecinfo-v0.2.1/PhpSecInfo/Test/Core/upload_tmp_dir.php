fbc6e16c90829c497b2f962d1a88177b1402d8ee
ZerooCool <g2pc@visionduweb.com>
2019-08-11 14:21:15 +0200

    #6 Amélioration du message de la Notice

--- 20070406-phpsecinfo-v0.2.1/PhpSecInfo/Test/Core/upload_tmp_dir.php
+++ 20070406-phpsecinfo-v0.2.1/PhpSecInfo/Test/Core/upload_tmp_dir.php
@@ -44,8 +44,7 @@ class PhpSecInfo_Test_Core_Upload_Tmp_Dir extends PhpSecInfo_Test_Core
      * We are disabling this function on Windows OSes right now until
      * we can be certain of the proper way to check world-readability
      *
-     * @return bool
-     * https://github.com/matomo-org/plugin-SecurityInfo/commit/6fa8ce0729b47bfd72f108f524bdeab1b77d2226#diff-af85a9dee2b2562f601a8f87c7f345d7
+     * @return bool https://github.com/matomo-org/plugin-SecurityInfo/commit/6fa8ce0729b47bfd72f108f524bdeab1b77d2226#diff-af85a9dee2b2562f601a8f87c7f345d7
      */
     function isTestable()
     {
@@ -90,12 +89,12 @@ class PhpSecInfo_Test_Core_Upload_Tmp_Dir extends PhpSecInfo_Test_Core
         parent::_setMessages();
 
         $this->setMessageForResult(PHPSECINFO_TEST_RESULT_NOTRUN, 'en', 'Test not run -- currently disabled on Windows OSes');
-        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_OK, 'en', 'upload_tmp_dir is enabled, which is the
-                                                recommended setting.');
+        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_OK, 'en', 'upload_tmp_dir is enabled, which is the recommended setting.');
         $this->setMessageForResult(PHPSECINFO_TEST_RESULT_WARN, 'en', 'Unable to retrieve file permissions on upload_tmp_dir');
-        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_NOTICE, 'en', 'upload_tmp_dir is disabled, or is set to a
-                                                common world-writable directory. This typically allows other users on this server
+        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_NOTICE, 'en', 'upload_tmp_dir is disabled, or you use the /tmp directory but
+                                                this naming is not allowed for this test and you must provide a custom directory, or is set
+                                                to a common world-writable directory (005). This typically allows other users on this server
                                                 to access temporary copies of files uploaded via your PHP scripts. You should set
-                                                upload_tmp_dir to a non-world-readable directory');
+                                                upload_tmp_dir to a non-world-readable directory (750)');
     }
 }
