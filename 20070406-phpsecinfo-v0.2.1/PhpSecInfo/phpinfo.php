94217ae6725e003b6cf29043f98c507bb5973113
ZerooCool <g2pc@visionduweb.com>
2019-08-12 22:21:04 +0200

    #1 #17 Commentaires pour la page phpinfo() et phpdocumentor

--- 20070406-phpsecinfo-v0.2.1/PhpSecInfo/phpinfo.php
+++ 20070406-phpsecinfo-v0.2.1/PhpSecInfo/phpinfo.php
@@ -1,4 +1,17 @@
 <?php
+/**
+* Page phpinfo() | PhpSecInfo/phpinfo.php
+*
+* @package PhpSecInfo
+* @author Zer00CooL <mail@visionduweb.com>
+*/
+
 echo '<a href="../">Revenir sur PhpSecInfo</a>';
 echo '<br/>';
+
+/**
+ * Fonction phpinfo() | PhpSecInfo/phpinfo.php
+ *
+ * @return string|boolean
+ */
 phpinfo();
