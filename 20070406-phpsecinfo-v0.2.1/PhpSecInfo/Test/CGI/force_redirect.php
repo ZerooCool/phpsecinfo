ae6a2bfc7a43390e7814c12ef9775024fba25e65
ZerooCool <g2pc@visionduweb.com>
2019-08-13 01:20:48 +0200

    #19 var passé à public
    var passé à public

--- 20070406-phpsecinfo-v0.2.1/PhpSecInfo/Test/CGI/force_redirect.php
+++ 20070406-phpsecinfo-v0.2.1/PhpSecInfo/Test/CGI/force_redirect.php
@@ -7,7 +7,7 @@
  */
 
 /**
- * require the PhpSecInfo_Test_Cgi class
+ * Require the PhpSecInfo_Test_Cgi class
  */
 require_once('PhpSecInfo/Test/Test_Cgi.php');
 
@@ -23,32 +23,27 @@ class PhpSecInfo_Test_Cgi_Force_Redirect extends PhpSecInfo_Test_Cgi
     /**
      * This should be a <b>unique</b>, human-readable identifier for this test
      *
-     * @var string
+     * @public string
      */
-    var $test_name = "force_redirect";
+    public $test_name = "force_redirect";
 
     /**
      * The recommended setting value
      *
-     * @var mixed
+     * @public mixed
      */
-    var $recommended_value = true;
-
-
+    public $recommended_value = true;
 
     function _retrieveCurrentValue()
     {
         $this->current_value = $this->getBooleanIniValue('cgi.force_redirect');
     }
 
-
     /**
      * Checks to see if cgi.force_redirect is enabled
-     *
      */
     function _execTest()
     {
-
         if ($this->current_value == $this->recommended_value) {
             return PHPSECINFO_TEST_RESULT_OK;
         }
@@ -56,17 +51,13 @@ class PhpSecInfo_Test_Cgi_Force_Redirect extends PhpSecInfo_Test_Cgi
         return PHPSECINFO_TEST_RESULT_WARN;
     }
 
-
-
     /**
      * Set the messages specific to this test
-     *
      */
     function _setMessages()
     {
         parent::_setMessages();
-
         $this->setMessageForResult(PHPSECINFO_TEST_RESULT_OK, 'en', "force_redirect is enabled, which is the recommended setting");
-        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_WARN, 'en', "force_redirect is disabled.  In most cases, this is a <strong>serious</strong> security vulnerability.  Unless you are absolutely sure this is not needed, enable this setting");
+        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_WARN, 'en', "force_redirect is disabled.  In most cases, this is a <strong>serious</strong> security vulnerability. Unless you are absolutely sure this is not needed, enable this setting");
     }
 }
