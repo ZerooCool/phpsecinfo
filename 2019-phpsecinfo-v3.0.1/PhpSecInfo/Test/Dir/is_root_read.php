5aea43f5eb3ab943b7ad01d06f10861644a82815
ZerooCool <g2pc@visionduweb.com>
2020-07-19 17:33:33 +0200

    Amélioration de la définition du test Dir/is_root_read.php
    Description améliorée pour l'anglais et le français.
    Ajout d'une traduction en Russe.
    
    Mise à jour du CHANGELOG.

--- 2019-phpsecinfo-v3.0.1/PhpSecInfo/Test/Dir/is_root_read.php
+++ 2019-phpsecinfo-v3.0.1/PhpSecInfo/Test/Dir/is_root_read.php
@@ -13,8 +13,6 @@ require_once ('PhpSecInfo/Test/Test_Dir.php');
 /**
  * Test class for function is_self_write
  * Checks if file permissions are proper for security.
- *
- * @author Glenn S Crystal <glenn@gcosoftware.com>
  */
 class PhpSecInfo_Test_Dir_Is_Root_Read extends PhpSecInfo_Test_Dir
 {
@@ -63,18 +61,28 @@ class PhpSecInfo_Test_Dir_Is_Root_Read extends PhpSecInfo_Test_Dir
     }
 
     /**
-     * Set the messages specific to this test
+     * Set the messages specific to this test.
      */
     function _setMessages()
     {
         parent::_setMessages();
+        /* en */
         $this->setMessageForResult(PHPSECINFO_TEST_RESULT_OK, 'en', "No permissions granted for the root ('/') directory. This is the most secure setup.");
+        /* L'affichage du message Warn n'est pas défini. */
         $this->setMessageForResult(PHPSECINFO_TEST_RESULT_WARN, 'en', "Write permission enabled for the root ('/') directory! You should never allow writing outside your www base.");
-        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_NOTICE, 'en', "Read permission enabled. You should under normal instances never allow Reading of the root ('/').");
+        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_NOTICE, 'en', "Normally, you should never allow the root ('/') to be read. You should never allow writing outside your www base. Enable and set the value of open_basedir to correct this problem from the PHP configuration.");
 
-        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_OK, 'fr', "Comment faire ? No permissions granted for the root ('/') directory. This is the most secure setup.");
-        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_WARN, 'fr', "Write permission enabled for the root ('/') directory! You should never allow writing outside your www base.");
-        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_NOTICE, 'fr', "Read permission enabled. You should under normal instances never allow Reading of the root ('/').");
+        /* fr */
+        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_OK, 'fr', "Aucune autorisation accordée pour le répertoire racine ('/'). Il s'agit de la configuration la plus sécurisée.");
+        /* L'affichage du message Warn n'est pas défini. */
+        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_WARN, 'fr', "Autorisation d'écriture activée pour le répertoire racine ('/')! Vous ne devez jamais autoriser l'écriture en dehors de votre base www.");
+        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_NOTICE, 'fr', "En temps normal, vous ne devriez jamais autoriser la lecture de la racine ('/'). Vous ne devez jamais autoriser l'écriture en dehors de votre base www. Activer et définir la valeur de open_basedir pour corriger ce problème depuis la configuration de PHP.");
+
+        /* ru */
+        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_OK, 'fr', "Нет прав для корневого каталога ('/'). Это самая безопасная конфигурация.");
+        /* L'affichage du message Warn n'est pas défini. */
+        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_WARN, 'fr', "Разрешение на запись активировано для корневого каталога ('/')! Вы никогда не должны позволять писать за пределами вашей базы www.");
+        $this->setMessageForResult(PHPSECINFO_TEST_RESULT_NOTICE, 'fr', "Обычно вы никогда не должны позволять читать корень ('/'). Вы никогда не должны позволять писать за пределами вашей базы www. Включите и установите значение open_basedir, чтобы исправить эту проблему из конфигурации PHP.");
         
     }
 }
